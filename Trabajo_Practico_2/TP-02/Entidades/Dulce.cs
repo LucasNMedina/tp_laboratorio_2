7c4fd47e9c688eddfd7f3047cffeddc84cc3010f
LucasNMedina <lucasnahuelmedina@hotmail.com>
2018-05-09 09:25:40 -0300

    Add files via upload

---
+++ Trabajo_Practico_2/TP-02/Entidades/Dulce.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2017
+{
+    public class Dulce : Producto
+    {
+        #region "Constructores"
+        public Dulce(EMarca marca, string CodigoDeBarras, ConsoleColor color)
+            : base(CodigoDeBarras, marca, color)
+        {
+
+        }
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// Los dulces tienen 80 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 80;
+            }
+        }
+        #endregion
+
+        #region "Métodos"
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("DULCE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
---
+++ Trabajo_Practico_2/TP-02/Entidades/Leche.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+
+namespace Entidades_2017
+{
+    public class Leche : Producto
+    {
+        #region "Enumerados"
+        public enum ETipo { Entera, Descremada }
+        #endregion
+
+        ETipo _tipo;
+
+        #region "Constructores"
+        /// <summary>
+        /// Por defecto, TIPO será ENTERA
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="patente"></param>
+        /// <param name="color"></param>
+        public Leche(EMarca marca, string codigoDeBarra, ConsoleColor color)
+            : this(marca, codigoDeBarra, color, ETipo.Entera)
+        {
+        }
+
+        public Leche(EMarca marca, string codigoDeBarra, ConsoleColor color, ETipo tipo)
+            : base(codigoDeBarra, marca, color)
+        {
+            this._tipo = tipo;
+        }
+        #endregion
+
+        #region "Propiedades"
+        /// <summary>
+        /// Las leches tienen 20 calorías
+        /// </summary>
+        protected override short CantidadCalorias
+        {
+            get
+            {
+                return 20;
+            }
+        }
+        #endregion
+
+        #region "Métodos"
+        public override string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("LECHE");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("CALORIAS : " + this.CantidadCalorias);
+            sb.AppendLine("TIPO : " + this._tipo);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+        #endregion
+    }
+}
